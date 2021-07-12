65c8e3aa687658ebf3f246f8ff77aa7dd5cec7ac
Doyoung.Jung <79549647+horizonn03@users.noreply.github.com>
2021-07-12 13:42:27 +0900

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,14 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY,username);
+    const savedUsername = localStorage.getItem(USERNAME_KEY);
+    alert("Hello " + savedUsername + "님 Welcome to my Naeflix!");
+    location.href="file:///Users/doyoung/Desktop/1st-assignment/introduce.html";
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
